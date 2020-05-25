968635ba9c660a4e04886bbed2a74f251b9ac389
clement-mwendwa <clementmatundu@gmail.com>
2020-05-25 14:46:21 +0300

    Add formatting error declaration

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -7,3 +7,11 @@ export interface IndentationOptions {
     useSpaces?: boolean
     tabSize?: number
 }
+
+export class FormattingError extends Error {
+    constructor() {
+        super('Formatting Error');
+
+        Object.setPrototypeOf(this, FormattingError.prototype);
+    }
+}
