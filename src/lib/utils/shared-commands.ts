b2b5c79aae45c96b9f2b5a5963839260bffeec25
cmwendwa <clementm916@gmail.com>
2021-05-14 12:38:49 +0300

    Return 'unused' statement

--- src/lib/utils/shared-commands.ts
+++ src/lib/utils/shared-commands.ts
@@ -3,5 +3,5 @@ import { Range, SnippetString, window } from "vscode"
 export async function insertLine(line: string, insertionLocation: Range) {
     let snippet = new SnippetString(line);
 
-    window?.activeTextEditor?.insertSnippet(snippet, insertionLocation);
+    return window?.activeTextEditor?.insertSnippet(snippet, insertionLocation);
 }
